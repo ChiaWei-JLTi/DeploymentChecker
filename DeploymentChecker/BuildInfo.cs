e20f2cdd85b07b9236c13175030c02771649856b
Woo Chia Wei <Woo_ChiaWei@jltasia.com>
2016-12-15 09:51:58 +0800

    Prototype

---
+++ DeploymentChecker/BuildInfo.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Reflection;
+using System.Xml.Linq;
+
+public class BuildInfo
+{
+    public string Tenant { get; set; }
+    public string BuildVersion { get; set; }
+    public string Server { get; set; }
+    public string Database { get; set; }
+//    public string UserId { get; set; }
+//    public string Password { get; set; }
+    public string IdentityServer { get; set; }
+    public string CreatedTime { get; set; }
+    public bool HasError { get; set; } = false;
+
+    public void ReadDbSetting(string configFile)
+    {
+        try 
+        {
+            var doc = XDocument.Load(configFile);
+            var xmlAttr = doc.Descendants("connectionStrings")
+                             .Descendants("add")
+                             .Attributes().First(x => x.Name=="connectionString");
+            var sqlConnString = new SqlConnectionStringBuilder(xmlAttr.Value);
+
+            Server = sqlConnString.DataSource;
+            Database = sqlConnString.InitialCatalog;
+//            UserId = sqlConnString.UserID;
+//            Password = sqlConnString.Password;
+        }
+        catch
+        {
+            HasError = false;
+        }
+    }
+
+    public void ReadIdServerSetting(string configFile)
+    {
+        try 
+        {
+            var doc = XDocument.Load(configFile);
+            var xmlAttr = doc.Descendants("appSettings")
+                            .Descendants("add")
+                            .First(x => x.Attributes().Any(y => y.Name == "key" && y.Value == "authority"))
+                            .Attributes()
+                            .First(x => x.Name == "value");
+
+            IdentityServer = xmlAttr.Value;
+        }
+        catch
+        {
+            HasError = false;
+        }
+    }
+
+    public static List<string> GetTitles()
+    {
+        return GetProperties().Select(x => x.Name).ToList();
+    }
+
+    public static List<string> GetContents(BuildInfo buildInfo)
+    {
+        return GetProperties().Select(x => (string) x.GetValue(buildInfo) ?? "").ToList();
+    }
+
+    private static IEnumerable<PropertyInfo> GetProperties()
+    {
+        return typeof(BuildInfo).GetProperties().Where(x => x.PropertyType == typeof(string));
+    }
+}
+
+
---
+++ DeploymentChecker/Report.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Diagnostics;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+
+public static class Report
+{
+    public static void Run()
+    {
+        var projectsFolder = ConfigurationManager.AppSettings["ProjectsFolder"];
+        var projectName = ConfigurationManager.AppSettings["ProjectName"];
+        var idServerRelativePath = ConfigurationManager.AppSettings["IdServerSettingPath"];
+        var dbSettingRelativePath = ConfigurationManager.AppSettings["DbSettingPath"];
+        var builds = new List<BuildInfo>();
+
+        foreach(var projectFolder in Directory.GetDirectories(projectsFolder))
+        {
+            if(!Path.GetFileName(projectFolder).ToLower().Contains(projectName.ToLower()))
+                continue;
+
+            foreach (var buildFolder in Directory.GetDirectories(projectFolder).Where(x => x.ToLower().Contains(projectName.ToLower())))
+            {
+                if(!Path.GetFileName(buildFolder).ToLower().Contains(projectName.ToLower()))
+                    continue;
+
+                var build = new BuildInfo
+                {
+                    Tenant = Path.GetFileName(projectFolder),
+                    BuildVersion = Path.GetFileName(buildFolder),
+                    CreatedTime = Directory.GetCreationTime(projectFolder).ToString(CultureInfo.InvariantCulture)
+                };
+
+                var idServerPath = $"{buildFolder}\\{idServerRelativePath}";
+                var dbSettingPath = $"{buildFolder}\\{dbSettingRelativePath}";
+                build.ReadIdServerSetting(idServerPath);
+                build.ReadDbSetting(dbSettingPath);
+                builds.Add(build);
+            }
+        }
+
+        Generate(builds);
+    }
+
+    public static void Generate(IEnumerable<BuildInfo> buildInfos)
+    {
+        var reportFolder = Path.Combine(Directory.GetCurrentDirectory(), "Report");
+        if (!Directory.Exists(reportFolder))
+            Directory.CreateDirectory(reportFolder);
+
+        var reportName = Path.Combine(reportFolder, $"Report_{DateTime.Now:yyyyMMdd_HHmmss}.html");
+
+        using (var writer = File.CreateText(reportName)) 
+        {
+            writer.WriteLine(@"<!DOCTYPE html>
+<html>
+<head>
+<title>Report</title>
+<style>
+    table {
+    border-collapse: collapse;
+    max-width: 100%;
+    white-space:nowrap;
+}
+th, td {
+    text-align: center;
+    padding-top: 5px;
+    padding-right: 15px;
+    padding-bottom: 5px;
+    padding-left: 15px;
+}
+tr:nth-child(even){background-color: #f2f2f2}
+th {
+    background-color: #4CAF50;
+    color: white;
+}
+.error{
+    color: red;
+}
+</style>
+</head>
+<body>
+<h1>
+Overview of Deployments
+</h1>
+<table>");
+
+            //Titles
+            writer.WriteLine("<tr>");
+            foreach (var title in BuildInfo.GetTitles()) 
+            {
+                writer.WriteLine($"<th>{title}</th>");
+            }
+            writer.WriteLine("</tr>");
+
+            //Datas
+            foreach (var buildInfo in buildInfos) 
+            {
+                writer.WriteLine(buildInfo.HasError ? "<tr class=\"error\">" : "<tr>");
+                foreach (var data in BuildInfo.GetContents(buildInfo)) 
+                {
+                    writer.WriteLine($"<td>{data}</td>");
+                }
+                writer.WriteLine("</tr>");
+            }
+
+            writer.WriteLine(@"</table>
+</body>
+</html>");
+
+            Process.Start(reportName);
+        }
+    }
+
+    
+}
